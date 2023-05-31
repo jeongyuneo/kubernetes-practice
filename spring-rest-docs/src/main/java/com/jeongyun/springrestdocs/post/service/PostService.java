9929835453fcab684a6492da656e65c1d7b79857
jeonyuneo <piq2255@gmail.com>
2023-05-31 23:56:11 +0900

    [spring-rest-docs] feat: PostService - create 메소드 추가

---
+++ spring-rest-docs/src/main/java/com/jeongyun/springrestdocs/post/service/PostService.java
@@ -0,0 +1,13 @@
+package com.jeongyun.springrestdocs.post.service;
+
+import com.jeongyun.springrestdocs.post.controller.dto.PostRequest;
+import com.jeongyun.springrestdocs.post.controller.dto.PostResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    public PostResponse create(PostRequest postRequest) {
+        return null;
+    }
+}
