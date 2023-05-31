07adab0d27d71ca8ad8df705a9582f96f188021b
jeonyuneo <piq2255@gmail.com>
2023-05-31 23:46:02 +0900

    [spring-rest-docs] chore: 프로젝트 세팅

---
+++ spring-rest-docs/src/test/java/com/jeongyun/springrestdocs/SpringRestDocsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jeongyun.springrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
