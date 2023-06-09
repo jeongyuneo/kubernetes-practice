f8ba36b6f98c2e4a2730655cb9eef846bfaf6dff
jeonyuneo <piq2255@gmail.com>
2023-06-09 20:18:59 +0900

    [spring-multi-module] chore: 프로젝트 세팅

---
+++ spring-multi-module/src/main/java/com/jeongyun/springmultimodule/SpringMultiModuleApplication.java
@@ -0,0 +1,12 @@
+package com.jeongyun.springmultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultiModuleApplication.class, args);
+    }
+}
---
+++ spring-multi-module/src/test/java/com/jeongyun/springmultimodule/SpringMultiModuleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jeongyun.springmultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
