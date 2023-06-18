d5fb9cb1c9ebf2771614b8417c9055f1d574a0ca
jeonyuneo <piq2255@gmail.com>
2023-06-18 18:01:23 +0900

    [spring-websocket] feat: ChattingController 추가

---
+++ spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/controller/ChattingController.java
@@ -0,0 +1,23 @@
+package com.jeongyuneo.springwebsocket.controller;
+
+import com.jeongyuneo.springwebsocket.dto.ChattingRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class ChattingController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chattings/{chattingId}/messages")
+    public void chat(@DestinationVariable Long chattingId, ChattingRequest chattingRequest) {
+        log.info("Message [{}] send by member: {} to chatting room: {}", chattingRequest.getContent(), chattingRequest.getSenderId(), chattingId);
+        simpMessagingTemplate.convertAndSend("/subscription/rooms/" + chattingId, chattingRequest.getContent());
+    }
+}
