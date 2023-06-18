43c4f74eed1002de4a2a15a18eea26ee29ea6819
jeonyuneo <piq2255@gmail.com>
2023-06-18 18:01:11 +0900

    [spring-websocket] feat: WebSocketConfig 추가

---
+++ spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.jeongyuneo.springwebsocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chattings")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/subscription");  // (1) 메시지 구독 요청: 메시지 송신
+        registry.setApplicationDestinationPrefixes("/publication"); // (2) 메시지 발행 요청: 메시지 수신
+    }
+}
