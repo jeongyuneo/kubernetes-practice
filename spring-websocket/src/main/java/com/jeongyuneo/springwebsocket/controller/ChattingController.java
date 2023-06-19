7d099b76760a0dc50bd0e4d18712d8e21350e3ad
jeonyuneo <piq2255@gmail.com>
2023-06-19 17:07:05 +0900

    [spring-websocket] refactor: ChattingController 채팅 요청 경로변수명 수정

--- spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/controller/ChattingController.java
+++ spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/controller/ChattingController.java
@@ -1,6 +1,7 @@
 package com.jeongyuneo.springwebsocket.controller;
 
 import com.jeongyuneo.springwebsocket.dto.ChattingRequest;
+import com.jeongyuneo.springwebsocket.service.ChattingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -15,8 +16,8 @@ public class ChattingController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping("/chattings/{chattingId}/messages")
-    public void chat(@DestinationVariable Long chattingId, ChattingRequest chattingRequest) {
+    @MessageMapping("/chattings/{chattingRoomId}/messages")
+    public void chat(@DestinationVariable Long chattingRoomId, ChattingRequest chattingRequest) {
         log.info("Message [{}] send by member: {} to chatting room: {}", chattingRequest.getContent(), chattingRequest.getSenderId(), chattingId);
         simpMessagingTemplate.convertAndSend("/subscription/chattings/" + chattingId, chattingRequest.getContent());
     }
