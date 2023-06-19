2432360b183ab99e0962ad2bf30cb9e55efd383c
jeonyuneo <piq2255@gmail.com>
2023-06-19 17:56:09 +0900

    [spring-websocket] feat: ChattingService 채팅 목록 조회 기능 추가

--- spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/service/ChattingService.java
+++ spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/service/ChattingService.java
@@ -1,14 +1,19 @@
 package com.jeongyuneo.springwebsocket.service;
 
 import com.jeongyuneo.springwebsocket.dto.ChattingRequest;
+import com.jeongyuneo.springwebsocket.dto.ChattingRoomResponse;
 import com.jeongyuneo.springwebsocket.entity.Chatting;
 import com.jeongyuneo.springwebsocket.entity.ChattingRoom;
 import com.jeongyuneo.springwebsocket.entity.Member;
 import com.jeongyuneo.springwebsocket.repository.ChattingRoomRepository;
+import com.jeongyuneo.springwebsocket.repository.MemberChattingRoomRepository;
 import com.jeongyuneo.springwebsocket.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +21,7 @@ public class ChattingService {
 
     private final MemberRepository memberRepository;
     private final ChattingRoomRepository chattingRoomRepository;
+    private final MemberChattingRoomRepository memberChattingRoomRepository;
 
     @Transactional
     public void save(Long chattingId, ChattingRequest chattingRequest) {
@@ -30,5 +36,19 @@ public class ChattingService {
                 .build();
         chattingRoom.createChatting(chatting);
         chattingRoomRepository.save(chattingRoom);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ChattingRoomResponse> getAll(Long memberId) {
+        return memberChattingRoomRepository.findByMemberId(memberId)
+                .stream()
+                .map(chattingRoomMember -> {
+                    ChattingRoom chattingRoom = chattingRoomMember.getChattingRoom();
+                    return ChattingRoomResponse.builder()
+                            .chattingRoomId(chattingRoom.getId())
+                            .name(chattingRoom.getName())
+                            .build();
+                })
+                .collect(Collectors.toList());
     }
 }
