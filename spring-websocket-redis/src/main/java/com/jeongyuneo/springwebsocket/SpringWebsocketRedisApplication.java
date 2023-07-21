cf314b0a86c9464f8389b4e076e6714e0c470c91
jeonyuneo <piq2255@gmail.com>
2023-07-21 10:42:13 +0900

    [spring-websocket-redis] chore: 프로젝트 세팅

---
+++ spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/SpringWebsocketRedisApplication.java
@@ -0,0 +1,12 @@
+package com.jeongyuneo.springwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketRedisApplication.class, args);
+    }
+}
