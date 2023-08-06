fe5d0018b96ee3de861f53d322d6feceb5b30b62
jeongyuneo <piq2255@gmail.com>
2023-08-07 00:15:44 +0900

    [spring-websocket] fix: Member SQLDelete 어노테이션 sql 수정

--- spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/entity/Member.java
+++ spring-websocket/src/main/java/com/jeongyuneo/springwebsocket/entity/Member.java
@@ -15,7 +15,7 @@ import java.util.List;
 @SuperBuilder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE chatting SET deleted = true WHERE id=?")
+@SQLDelete(sql = "UPDATE member SET deleted = true WHERE id=?")
 @AttributeOverride(name = "id", column = @Column(name = "member_id"))
 @Entity
 public class Member extends BaseEntity {
