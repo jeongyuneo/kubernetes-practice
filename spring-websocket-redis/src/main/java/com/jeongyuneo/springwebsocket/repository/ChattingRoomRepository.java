4f9562a3c00779af09b95b2c7c5fbbd6153b6f05
jeongyuneo <piq2255@gmail.com>
2023-08-07 00:03:48 +0900

    [spring-websocket-redis] feat: ChattingRoomRepository 채팅방 생성 기능 추가

--- spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/repository/ChattingRoomRepository.java
+++ spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/repository/ChattingRoomRepository.java
@@ -1,10 +1,16 @@
 package com.jeongyuneo.springwebsocket.repository;
 
+import com.jeongyuneo.springwebsocket.entity.ChattingRoom;
+import com.jeongyuneo.springwebsocket.service.RedisSubscriber;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,9 +19,27 @@ import java.util.Map;
 @Repository
 public class ChattingRoomRepository {
 
+    private static final String CHATTING_ROOM = "CHATTING_ROOM";
     private static final Map<String, ChannelTopic> TOPICS = new HashMap<>();
+
+    private final RedisMessageListenerContainer redisMessageListenerContainer;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisSubscriber redisSubscriber;
+
+    private HashOperations<String, String, ChattingRoom> opsHashChattingRoom;
+
+    @PostConstruct
+    private void init() {
+        opsHashChattingRoom = redisTemplate.opsForHash();
+    }
 
     public ChannelTopic getTopic(String chattingRoomId) {
         return TOPICS.get(chattingRoomId);
+    }
+
+    public void createChattingRoom(String name) {
+        ChattingRoom chattingRoom = ChattingRoom.from(name);
+        opsHashChattingRoom.put(CHATTING_ROOM, chattingRoom.getId().toString(), chattingRoom);
+        log.info("create new chatting room '{}'", chattingRoom.getId());
     }
 }
