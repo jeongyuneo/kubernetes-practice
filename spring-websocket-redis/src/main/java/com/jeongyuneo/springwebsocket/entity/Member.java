881305557cb7865f18e7bf76e3c4cd921634775a
jeongyuneo <piq2255@gmail.com>
2023-08-07 00:14:44 +0900

    [spring-websocket-redis] refactor: Member 미사용 필드 및 메소드 삭제

--- spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/entity/Member.java
+++ spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/entity/Member.java
@@ -1,32 +1,24 @@
 package com.jeongyuneo.springwebsocket.entity;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @SuperBuilder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE chatting SET deleted = true WHERE id=?")
+@SQLDelete(sql = "UPDATE member SET deleted = true WHERE id = ?")
 @AttributeOverride(name = "id", column = @Column(name = "member_id"))
 @Entity
 public class Member extends BaseEntity {
 
     private String name;
-
-    @Builder.Default
-    @OneToMany(mappedBy = "member")
-    private final List<MemberChattingRoom> memberChattingRooms = new ArrayList<>();
-
-    public void createChatting(MemberChattingRoom memberChattingRoom) {
-        memberChattingRooms.add(memberChattingRoom);
-    }
 }
