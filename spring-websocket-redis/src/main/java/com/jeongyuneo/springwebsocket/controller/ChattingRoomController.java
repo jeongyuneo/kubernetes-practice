467fb287b2e83b497e296b4a493281a6f41edfc0
jeongyuneo <piq2255@gmail.com>
2023-08-07 00:06:27 +0900

    [spring-websocket-redis] feat: ChattingRoomController 채팅방 입장 기능 추가

--- spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/controller/ChattingRoomController.java
+++ spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/controller/ChattingRoomController.java
@@ -3,10 +3,7 @@ package com.jeongyuneo.springwebsocket.controller;
 import com.jeongyuneo.springwebsocket.dto.ChattingRoomCreateRequest;
 import com.jeongyuneo.springwebsocket.service.ChattingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +15,10 @@ public class ChattingRoomController {
     @PostMapping
     public void createChattingRoom(@RequestBody ChattingRoomCreateRequest chattingRoomCreateRequest) {
         chattingService.createChattingRoom(chattingRoomCreateRequest);
+    }
+
+    @PostMapping("{chattingRoomId}")
+    public void enterChattingRoom(@PathVariable String chattingRoomId) {
+        chattingService.enterChattingRoom(chattingRoomId);
     }
 }
