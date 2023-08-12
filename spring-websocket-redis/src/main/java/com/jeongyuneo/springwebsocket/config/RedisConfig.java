74ef890d861c0674b0ae7f0e8aa73c903123a238
jeongyuneo <piq2255@gmail.com>
2023-08-12 17:40:43 +0900

    [spring-websocket-redis] feat: RedisConfig RedisConnectionFactory 빈 둥록

--- spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/config/RedisConfig.java
+++ spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package com.jeongyuneo.springwebsocket.config;
 
 import com.jeongyuneo.springwebsocket.service.RedisSubscriber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -15,6 +17,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     private static final String TOPIC_NAME = "topic";
+
+    @Value("${REDIS_HOST}")
+    private String host;
+
+    @Value("${REDIS_PORT}")
+    private int port;
 
     @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
@@ -38,6 +46,11 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
