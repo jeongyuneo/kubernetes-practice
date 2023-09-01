5681283cb95801c561aef07cd704e156bb125724
jeongyuneo <piq2255@gmail.com>
2023-09-01 19:00:21 +0900

    [spring-websocket-redis] refactor: RedisSubscriber JsonParser 의존성 추가

--- spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/service/RedisSubscriber.java
+++ spring-websocket-redis/src/main/java/com/jeongyuneo/springwebsocket/service/RedisSubscriber.java
@@ -1,8 +1,7 @@
 package com.jeongyuneo.springwebsocket.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jeongyuneo.springwebsocket.dto.Message;
+import com.jeongyuneo.springwebsocket.util.JsonParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.MessageListener;
@@ -15,22 +14,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisSubscriber implements MessageListener {
 
-    private final ObjectMapper objectMapper;
-    private final RedisTemplate redisTemplate;
     private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisTemplate redisTemplate;
+    private final JsonParser jsonParser;
 
     @Override
     public void onMessage(org.springframework.data.redis.connection.Message message, byte[] pattern) {
-        Message messageRequest = toMessageRequest((String) redisTemplate.getStringSerializer().deserialize(message.getBody()));
+        Message messageRequest = jsonParser.toMessageRequest((String) redisTemplate.getStringSerializer().deserialize(message.getBody()));
         messagingTemplate.convertAndSend("/subscription/chattings/rooms/" + messageRequest.getChattingRoomId(), messageRequest);
         log.info("Message [{}] send by member: {} to chatting room: {}", messageRequest.getContent(), messageRequest.getSenderId(), messageRequest.getChattingRoomId());
-    }
-
-    private Message toMessageRequest(String chattingMessage) {
-        try {
-            return objectMapper.readValue(chattingMessage, Message.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
