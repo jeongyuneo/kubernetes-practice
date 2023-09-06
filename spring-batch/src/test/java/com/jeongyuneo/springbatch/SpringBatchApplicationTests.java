3cc481e982fb4231b3a7e17daa3e41e67bf91ddc
jeongyuneo <piq2255@gmail.com>
2023-09-06 11:35:45 +0900

    [spring-batch] chore: 프로젝트 세팅

---
+++ spring-batch/src/test/java/com/jeongyuneo/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jeongyuneo.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
