116039e3fc32fbd8960e52876b53f71e92bd81ae
jeongyuneo <piq2255@gmail.com>
2023-10-22 21:22:14 +0900

    [querydsl] feat: Hello 엔티티 추가

---
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/entity/Hello.java
@@ -0,0 +1,16 @@
+package com.jeongyuneo.querydsl.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
