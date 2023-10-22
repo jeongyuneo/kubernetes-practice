1f5c14b94953edb807288ca05e6f339e211d9f2f
jeongyuneo <piq2255@gmail.com>
2023-10-22 22:07:58 +0900

    [querydsl] test: querydsl 실행 테스트 추가

--- querydsl/src/test/java/com/jeongyuneo/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/com/jeongyuneo/querydsl/QuerydslApplicationTests.java
@@ -1,12 +1,37 @@
 package com.jeongyuneo.querydsl;
 
+import com.jeongyuneo.querydsl.entity.Hello;
+import com.jeongyuneo.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+    @Autowired
+    EntityManager entityManager;
+
     @Test
     void contextLoads() {
+        // given
+        Hello hello = new Hello();
+        entityManager.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(entityManager);
+        QHello qHello = QHello.hello;
+        // when
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+        // then
+        assertThat(result).isEqualTo(hello);
     }
 }
