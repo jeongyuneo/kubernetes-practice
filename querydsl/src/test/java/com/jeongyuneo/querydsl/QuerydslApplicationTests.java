3a59c6bbadbc0fd6c941ff7741a6423bb470da73
jeongyuneo <piq2255@gmail.com>
2023-10-23 16:27:40 +0900

    [querydsl] test: querydsl 실행 테스트 Commit 어노테이션 추가

--- querydsl/src/test/java/com/jeongyuneo/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/com/jeongyuneo/querydsl/QuerydslApplicationTests.java
@@ -6,12 +6,14 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Commit
 @Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
