28e3805d986240935b48b775868d6486cec09b01
jeongyuneo <piq2255@gmail.com>
2023-10-24 19:02:39 +0900

    [querydsl] test: JPQL 멤버 조회 테스트 추가

---
+++ querydsl/src/test/java/com/jeongyuneo/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,50 @@
+package com.jeongyuneo.querydsl;
+
+import com.jeongyuneo.querydsl.entity.Member;
+import com.jeongyuneo.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @BeforeEach
+    void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+    }
+
+    @Test
+    void JPQL을_이용해_멤버1을_찾는다() {
+        // given
+        String query = "select m from Member m where m.username = :username";
+        // when
+        Member findMember = entityManager.createQuery(query, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+        // then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
