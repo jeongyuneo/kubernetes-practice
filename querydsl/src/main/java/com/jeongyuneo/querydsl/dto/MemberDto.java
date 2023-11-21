06664d6fa155e0db311ccaf5c7303874dbfcfbee
jeongyuneo <piq2255@gmail.com>
2023-11-21 14:21:28 +0900

    [querydsl] feat: MemberDto 기본 생성자 추가

--- querydsl/src/main/java/com/jeongyuneo/querydsl/dto/MemberDto.java
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/dto/MemberDto.java
@@ -2,9 +2,11 @@ package com.jeongyuneo.querydsl.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberDto {
 
     private String username;
