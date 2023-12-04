9d9e5bf896d29728eb482a69c7eba7690aeae34f
jeongyuneo <piq2255@gmail.com>
2023-12-04 21:17:51 +0900

    [querydsl] feat: JPAMemberRepository Querydsl 메소드 추가

--- querydsl/src/main/java/com/jeongyuneo/querydsl/repository/MemberJPARepository.java
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/repository/MemberJPARepository.java
@@ -1,20 +1,21 @@
 package com.jeongyuneo.querydsl.repository;
 
 import com.jeongyuneo.querydsl.entity.Member;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static com.jeongyuneo.querydsl.entity.QMember.member;
+
 @Repository
+@RequiredArgsConstructor
 public class MemberJPARepository {
 
     private final EntityManager entityManager;
-
-    public MemberJPARepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    private final JPAQueryFactory queryFactory;
 
     public void save(Member member) {
         entityManager.persist(member);
@@ -34,5 +35,18 @@ public class MemberJPARepository {
         return entityManager.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
+    }
+
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(member)
+                .fetch();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
     }
 }
