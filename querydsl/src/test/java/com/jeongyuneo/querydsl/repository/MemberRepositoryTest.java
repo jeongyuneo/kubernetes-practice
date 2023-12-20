9c27d7a6fda0263ee33c3178049219d140cce9f3
jeongyuneo <piq2255@gmail.com>
2023-12-21 00:05:46 +0900

    [querydsl] test: MemberRepository Querydsl 메소드 동작 테스트 추가

--- querydsl/src/test/java/com/jeongyuneo/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/com/jeongyuneo/querydsl/repository/MemberRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.jeongyuneo.querydsl.repository;
 
+import com.jeongyuneo.querydsl.dto.MemberSearchCondition;
+import com.jeongyuneo.querydsl.dto.MemberTeamDto;
 import com.jeongyuneo.querydsl.entity.Member;
+import com.jeongyuneo.querydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -12,6 +18,9 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @Transactional
 @SpringBootTest
 class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager entityManager;
 
     @Autowired
     MemberRepository memberRepository;
@@ -28,5 +37,28 @@ class MemberRepositoryTest {
                 () -> assertThat(memberRepository.findAll()).containsExactly(member),
                 () -> assertThat(memberRepository.findByUsername(member.getUsername())).containsExactly(member)
         );
+    }
+
+    @Test
+    void Querydsl을_이용해_35살이상_40살이하이고_팀B에_속한_회원을_검색한다() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+        MemberSearchCondition condition = new MemberSearchCondition(null, "teamB", 35, 40);
+        // when
+        List<MemberTeamDto> result = memberRepository.search(condition);
+        // then
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
