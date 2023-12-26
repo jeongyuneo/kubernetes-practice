b34b7c6b68bb78f25b7ac7e34ed09e6bdc0a8ffd
jeongyuneo <piq2255@gmail.com>
2023-12-26 22:03:49 +0900

    [querydsl] test: Querydsl 단순 카운트 조회 테스트 추가

--- querydsl/src/test/java/com/jeongyuneo/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/com/jeongyuneo/querydsl/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import com.jeongyuneo.querydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -60,5 +62,33 @@ class MemberRepositoryTest {
         List<MemberTeamDto> result = memberRepository.search(condition);
         // then
         assertThat(result).extracting("username").containsExactly("member4");
+    }
+
+    @Test
+    void Querydsl을_이용해_사이즈3인_페이지의_0번째_회원페이지를_검색한다() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition(null, null, null, null);
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        // when
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
+        // then
+        assertAll(
+                () -> assertThat(result).hasSize(3),
+                () -> assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3")
+        );
     }
 }
