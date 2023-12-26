7e6a9a3e93ea818ca40bc67ab8a7c39e68698080
jeongyuneo <piq2255@gmail.com>
2023-12-26 22:01:50 +0900

    [querydsl] feat: CustomMemberRepository Querydsl 단순 카운트 조회 메소드 추가

--- querydsl/src/main/java/com/jeongyuneo/querydsl/repository/CustomMemberRepository.java
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/repository/CustomMemberRepository.java
@@ -2,10 +2,14 @@ package com.jeongyuneo.querydsl.repository;
 
 import com.jeongyuneo.querydsl.dto.MemberSearchCondition;
 import com.jeongyuneo.querydsl.dto.MemberTeamDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CustomMemberRepository {
 
     List<MemberTeamDto> search(MemberSearchCondition condition);
+
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
 }
