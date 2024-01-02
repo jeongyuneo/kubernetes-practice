21b1b5b3683349359e8116a6f242a971783efdcd
jeongyuneo <piq2255@gmail.com>
2024-01-02 21:45:05 +0900

    [querydsl] feat: MemberController 회원 검색 복잡한 페이징 처리 메소드 추가

--- querydsl/src/main/java/com/jeongyuneo/querydsl/controller/MemberController.java
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/controller/MemberController.java
@@ -30,4 +30,9 @@ public class MemberController {
     public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
         return memberRepository.searchPageSimple(condition, pageable);
     }
+
+    @GetMapping("/v3")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
+    }
 }
