8819210c32d074ea11b560c84edfb1b518739ac4
jeongyuneo <piq2255@gmail.com>
2024-01-02 21:41:37 +0900

    [querydsl] feat: MemberController 회원 검색 페이징 처리 메소드 추가

--- querydsl/src/main/java/com/jeongyuneo/querydsl/controller/MemberController.java
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.jeongyuneo.querydsl.controller;
 import com.jeongyuneo.querydsl.dto.MemberSearchCondition;
 import com.jeongyuneo.querydsl.dto.MemberTeamDto;
 import com.jeongyuneo.querydsl.repository.MemberJPARepository;
+import com.jeongyuneo.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,15 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJPARepository memberJPARepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJPARepository.search(condition);
+    }
+
+    @GetMapping("/v2")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
     }
 }
