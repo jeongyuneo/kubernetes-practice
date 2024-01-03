621e88d559a7facc7240de01eace15f93f655202
jeongyuneo <piq2255@gmail.com>
2024-01-03 22:30:37 +0900

    [querydsl] feat: MemberRepository QuerydslPredicateExecutor 상속

--- querydsl/src/main/java/com/jeongyuneo/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/com/jeongyuneo/querydsl/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package com.jeongyuneo.querydsl.repository;
 
 import com.jeongyuneo.querydsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);
 }
