52312b64bb8f1c9e35c60f9083ab738e90809aaa
jeongyuneo <piq2255@gmail.com>
2024-01-03 22:45:20 +0900

    [querydsl] test: MemberRepository QuerydslPredicateExecutor 테스트 추가

--- querydsl/src/test/java/com/jeongyuneo/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/com/jeongyuneo/querydsl/repository/MemberRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static com.jeongyuneo.querydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -91,5 +92,31 @@ class MemberRepositoryTest {
                 () -> assertThat(result).hasSize(3),
                 () -> assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3")
         );
+    }
+
+    @Test
+    void Querydsl을_이용해_10살이상_40살이하이고_이름이_member1인_회원을_검색한다() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+        // when
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40).and(member.username.eq("member1"))
+        );
+        // then
+        for (Member findMember : result) {
+            System.out.println(findMember);
+        }
     }
 }
